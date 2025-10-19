d192ddf1c10bed47f86a69b67cbbaa5b5be05a6f
marseneaul <arseneault.max@gmail.com>
2025-10-18 18:10:00 -0700

    fix: Remove unused colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,9 +2,6 @@ export const BLACK = "rgba(0, 0, 0, 1)";
 export const WHITE = "rgba(255, 255, 255, 1";
 export const RED = "rgba(255, 0, 0, 1)";
 
-export const LIGHT_GRAY = "rgba(245, 245, 245, 1)";
-export const BLOOD_RED = "rgba(136, 8, 8, 1)";
-
 export const DEFAULT_NODE_COLOR = BLACK;
 export const PULSING_NODE_COLOR = RED;
 
